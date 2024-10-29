48bcd7e8ce1bdad4440e552f4724d212e182ea9b
mchaery <156283213+mchaery@users.noreply.github.com>
2024-10-29 15:46:10 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,18 +1,18 @@
-%% 케이스선택 
+%% select case
 clc, clear
-Video_view = input('case 방향을 입력하세요(R/B) : ','s');
+Video_view = input('enter the case direction(R/B) : ','s');
 if (Video_view=='r')||(Video_view=='R')
     Video_view = 'R';
 elseif (Video_view == 'b')||(Video_view=='B')
     Video_view = 'B';
 else
-    disp('방향을 다시 입력하세요')
+    disp('Please re-enter the direction.')
     pause(3)
     return
 end
 
-s1 = input('첫번째 케이스의 번호를 입력하세요(1/2/3): ','s');
-s2 = input('두번째 케이스의 번호를 입력하세요(1/2/3): ','s');
+s1 = input('Enter the number for the first case (1/2/3):','s');
+s2 = input('Enter the number for the second case (1/2/3):','s');
 
 if (Video_view == 'R')
     s = 'Right_case_';
@@ -22,12 +22,12 @@ end
 
 Case_name1 = append(s,s1);
 Case_name2 = append(s,s2);
-%% 계산
+%% calculation
 
 calculation(Case_name1, Video_view)
 calculation(Case_name2, Video_view)
 
-%% 그래프
+%% visualize, save data/graph
 if (Video_view == 'R')
     show_graph_r(Case_name1,Case_name2)
 else
